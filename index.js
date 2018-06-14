096c6f6b3ee32cecc5105d29aa106b90323c4886
Johan 't Hart <johanthart@gmail.com>
2018-06-14 22:01:46 +0200

    Creating a mapper

--- index.js
+++ index.js
@@ -17,17 +17,19 @@ const counter = timeout => (type, sink) => {
 
 const makePrinter = id => {
     let talkBack;
+    let count = 0;
     return (type, data) => {
         if(type === 0) {
             talkBack = data;
             console.log(id, 'Starting... Talkback: ', data);
         }
         if(type === 1) {
-            console.log(id, 'Received', data);
-            if (data === 10) {
+            console.log(id, 'Received', count, data);
+            if (count === 10) {
                 console.log(id, 'Reached 10. Terminating.');
                 talkBack(2); // terminate at 10
             }
+            ++count;
         }
         if(type === 2) {
             console.log(id, 'End', data);
@@ -36,5 +38,12 @@ const makePrinter = id => {
     };
 };
 
-counter(100)(0, makePrinter(1));
-counter(200)(0, makePrinter(2));
+const map = transform => source => (_, sink) =>
+    source(0, (t, d) => t === 1
+        ? sink(1, transform(d))
+        : sink(t, d));
+
+
+
+map(i => i + 100)(counter(100))(0, makePrinter(1));
+map(i => 'Say it ' + i + ' times')(counter(200))(0, makePrinter(2));
